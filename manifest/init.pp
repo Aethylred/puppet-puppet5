a299c40789dc48f6985f1ac3cf118b58db4232ef
Aaron Hicks <aaron.hicks@niwa.co.nz>
2017-07-03 16:56:08 +1200

    Initial manifest

---
+++ manifest/init.pp
@@ -0,0 +1,15 @@
+# Puppet class
+# 
+# This is the base class that can be used to install and manage all the Puppet5 things in this module
+# 
+# @example Declaring the class
+#   include puppet
+#   
+# @param [String] hostname Hostname used to generate the certificate for server and agent
+# 
+
+class puppet(
+             String $hostname
+             ) {
+  # Do nothing right now ok
+}
