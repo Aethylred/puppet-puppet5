48598a1a9176a3edcf0de212690e7b6451f8e9a2
Aaron Hicks <aaron.hicks@niwa.co.nz>
2017-07-04 14:32:32 +1200

    A class to install the Puppet5 repositories

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,11 @@
 # puppet5 class
 # 
 # This class is used to install the Puppet 5 agent
+# Installing a puppet5 repo is required, however the puppet5::repos class is
+# not required as long as a repository is set up prior to calling this class
 # 
 # @example Declaring the class
-#   include puppet
+#   include puppet5
 #   
 # @param [String] certname The hostname used to generate the certificate for server and agent
 # 
