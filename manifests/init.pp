e59bbaba816a5593fa9f288927aad8ceeec0219b
Aaron Hicks <aaron.hicks@niwa.co.nz>
2017-07-04 15:04:14 +1200

    Install a package with variables from hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,9 +11,28 @@
 # 
 
 class puppet5(
-  String $certname
-  ) {
+  String $package,
+  String $version,
+  Variant[Boolean, Enum['true', 'false', 'installed', 'absent']] $ensure = 'installed', # lint:ignore:quoted_booleans
+) {
 
   include puppet5::oscheck
+
+  if $ensure {
+    $ensure_package = $version
+    $ensure_dir     = 'directory'
+    $ensure_file    = 'file'
+    $ensure_present = 'present'
+  } else {
+    $ensure_package = 'absent'
+    $ensure_dir     = 'absent'
+    $ensure_file    = 'absent'
+    $ensure_present = 'absent'
+  }
+
+  package{'puppet-agent':
+    ensure => $ensure_package,
+    name   => $package,
+  }
 
 }
--- spec/classes/puppet_spec.rb
+++ spec/classes/puppet_spec.rb
@@ -10,6 +10,13 @@ describe 'puppet5' do
       it { is_expected.to compile.with_all_deps }
       it { should contain_class('puppet5::oscheck') }
 
+      context "with no paramters" do
+        it { should contain_package('puppet-agent').with(
+          'ensure' => hiera.lookup('ensure_package'),
+          'name'   => hirea.lookup('package')
+        ) }
+      end
+
     end
   end
 end
